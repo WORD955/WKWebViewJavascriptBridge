fae5bd68e87ade416b293545dc611d6b188c1bbf
conversun <conversun@gmail.com>
2022-04-26 10:34:12 +0800

    fix: package

--- Package.swift
+++ Package.swift
@@ -5,19 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "WKWebViewJavascriptBridge",
+    platforms: [.iOS("14.0"),
+                .macOS("10.15"),
+                .watchOS("6.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "WKWebViewJavascriptBridge",
             targets: ["WKWebViewJavascriptBridge"]),
     ],
-    dependencies: [],
+//    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "WKWebViewJavascriptBridge",
-			dependencies: [],
-			path: "WKWebViewJavascriptBridge/"),
+            name: "WKWebViewJavascriptBridge"),
     ]
 )
